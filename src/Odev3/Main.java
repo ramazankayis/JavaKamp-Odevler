f9ded3a8a119da09201b55a542c505cdae3061d2
Ramazan KAYIŞ <11260047@ogrenci.firat.edu.tr>
2021-05-02 00:30:16 +0300

    odev3 son commit

--- src/Odev3/Main.java
+++ src/Odev3/Main.java
@@ -9,16 +9,16 @@ public class Main {
 		InstructorManager instructorManager = new InstructorManager();
 
 		System.out.println("***123******Öğrenciler**123****");
-		Student student1 = new Student(1, "Ramazan", "kayış", "0000000", "ramazank@gmail.com", "pppppp",
+		Student student1 = new Student(1, "Ramazan", "kayış", "00ss00000", "ramazank@gmail.com", "pppppp",
 				"87887889898", 2);
 		Student student2 = new Student(2, "rümeysa", "yavaş", "111111", "rümeysay@gmail.com", "oooooo",
 				"12121212121", 1);
 		Student[] students = { student1, student2 };
 		studentManager.getAllDetailStudents(students);
 
-		System.out.println("*123****Öğretmen*****Eğimen*123**");
+		System.out.println("*123****Öğssretmen*****Eğimen*123**");
 		Instructor instructor1 = new Instructor(3, "Engin", "Demiroğ", "9999999", "engind@gmail.com",
-				"45454545454545", 3, 20000);
+				"454545454ss54545", 3, 20000);
 		Instructor instructor2 = new Instructor(4, "Esra", "Sancak", "88888888", "esras@gmail.com",
 				"969696969699", 6, 12000);
 		Instructor[] instructors = { instructor1, instructor2 };
